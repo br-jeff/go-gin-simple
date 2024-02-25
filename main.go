97affff446b75a724d7d878f67e564749e2561f5
Jefferson Silva <jefferson_1996Th@hotmail.com>
2024-02-25 16:10:45 -0300

    set routes on main

--- main.go
+++ main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/br-jeff/go-gin-simple/routes"
 )
 
-func GetAllPlayers(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"id":   "1",
-		"name": "Neymar",
-	})
-}
-
 func main() {
-	r := gin.Default()
-
-	r.GET("/players", GetAllPlayers)
-
-	r.Run(":8080")
+	routes.HandleRequest()
 }
